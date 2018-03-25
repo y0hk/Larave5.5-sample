a98e522b179c7b80239a80f7b4da9d22cf823b45
vagrant <vagrant@homestead>
2018-03-25 06:30:11 +0000

    .gitignore is now working

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,6 +15,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     static $password;
+    static $seed = 0;
+
+    $faker->seed($seed++);
 
     return [
         'name' => $faker->name,
